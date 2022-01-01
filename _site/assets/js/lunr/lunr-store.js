73d0f282baa2210130b5c498fd72716777d3704f
Kiwon Yang <p2881p@naver.com>
2022-01-02 03:14:47 +0900

    Update Blog

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,4 +1,10 @@
 var store = [{
+        "title": "Test",
+        "excerpt":"01-1. Test      hi   오 이게 되네   print('hi')  'hi'   01-2. Test2      와우   ","categories": ["test"],
+        "tags": ["Test","test"],
+        "url": "/test/test/",
+        "teaser": null
+      },{
         "title": "[Python] 1. 레퍼런스 카운트와 가비지 컬렉션",
         "excerpt":"본 포스팅은 “윤성우의 열혈 파이썬 중급편” 책 내용을 기반으로 작성되었습니다. 잘못된 내용이 있을 경우 지적해 주시면 감사드리겠습니다. 1-1. 가비지 컬렉션 다음 코드를 보자. laugh = '푸헤헿' 우리는 이를 보고 “변수 laugh에 문자열 ‘푸헤헿’을 저장했다.”고 표현한다. 하지만 상기 코드는 다음과 같음을 알고 있어야 한다. “변수 laugh가 문자열 ‘푸헤헿’을 참조(Reference)한다.” 즉, 포스트잇...","categories": ["pythonmd"],
         "tags": ["python","reference count","garbage collection"],
@@ -9,12 +15,6 @@ var store = [{
         "excerpt":"본 포스팅은 “윤성우의 열혈 파이썬 중급편” 책 내용을 기반으로 작성되었습니다. 잘못된 내용이 있을 경우 지적해 주시면 감사드리겠습니다. 2-1. Immutable &amp; Mutable Mutable 객체: 객체가 지닌 값의 수정이 가능한 객체 Ex) 리스트, 딕셔너리 today = ['공부하자', '운동하자'] id(today) (결과) 1896625928712 today += ['요리하자', '일찍자자'] id(today) (결과) 1896625928712 Immutable 객체: 객체가 지닌...","categories": ["pythonmd"],
         "tags": ["python","immutable","mutable"],
         "url": "/pythonmd/pythonmd2/",
-        "teaser": null
-      },{
-        "title": "Test",
-        "excerpt":"01-1. Test      hi   오 이게 되네   print('hi')  'hi'   01-2. Test2      와우   ","categories": ["test"],
-        "tags": ["Test","test"],
-        "url": "/test/test/",
         "teaser": null
       },{
         "title": "[Python] 3. 깊은 복사와 얕은 복사",
